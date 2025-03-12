bf845889150a400b773469bed825871d7a954082
MauricioCa07 <carrillomauricio007@gmail.com>
2025-03-11 21:34:12 -0500

    oppening the end point

--- src/board/__init__.py
+++ src/board/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from board import modules
+from flask_cors import CORS
 
 def create_app():
     app = Flask(__name__)
 
+    CORS(app)
     app.register_blueprint(modules.bp)
     return app
 
